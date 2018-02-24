7b6c0193561610e49bc36cc6bcd4dfd52b049563
vinay2venkat <vinay2venkat@gmail.com>
2018-02-24 15:31:32 +0530

    Update yt.py

--- yt.py
+++ yt.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
+from pyvirtualdisplay import Display
 from time import sleep
+
+display = Display(visible=0, size=(800, 600))
+display.start()
+
 count = 0
 for i in range(2):
     driver = webdriver.Firefox()
